9683f84202f0408689c1a31943b70ede0fbd787d
Ivan Dmitriev <van4011@yandex.ru>
2019-10-28 19:02:40 +0300

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+a = input()
+con = sqlite3.connect(a)
+cur = con.cursor()
+result = cur.execute("""SELECT title FROM Films
+            WHERE year >= 1997 and genre IN (
+SELECT id FROM genres 
+    WHERE title = 'музыка' or title = 'анимация')""").fetchall()
+
+for elem in result:
+    print(elem[0])
+
+con.close()
